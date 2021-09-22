5d8e7a331c09aa49af1820a5107a5912520caf41
Pitec7 <chenxi.lin@hotmail.ch>
2021-09-22 14:44:56 +0200

    temporaire

---
+++ minichat_post.php
@@ -0,0 +1,19 @@
+<?php
+try
+{
+    // On se connecte à la base de donnée MySQL, ici tp_openclassrooms
+    $bdd = new PDO('mysql:host=localhost;dbname=tp_openclassrooms;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : ' . $e->getMessage());
+}
+
+// On récupère tout le contenu de la table minichat
+$reponse = $bdd->query('SELECT * FROM minichat');
+$message = $reponse->fetch();
+
+// Redirection vers minichat.php
+header('Location: minichat.php');
+?>
