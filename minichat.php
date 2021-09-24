61a84221a5e8cc02ffc23a431056d2adea2d3732
Pitec7 <chenxi.lin@hotmail.ch>
2021-09-24 09:45:16 +0200

    Correction du site OpenClassrooms

--- minichat.php
+++ minichat.php
@@ -1,54 +1,48 @@
-<?php
-// Démarrage de la session
-session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="fr">
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mini-chat</title>
+    </head>
+    <style>
+    form
+    {
+        text-align:center;
+    }
+    </style>
+    <body>
+    
+    <form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <input type="submit" value="Envoyer" />
+	</p>
+    </form>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
-    <title>Minichat</title>
-</head>
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
-<body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <form class="col-8 col-md-6 col-lg-4" action="minichat_post.php" method="post">
-                <div class="my-3 row form-group">
-                    <label for="pseudo" class="col-3 col-form-label">Pseudo:</label>
-                    <div class="col-9">
-                        <input type="text" class="form-control" name="pseudo" id="pseudo" />
-                    </div>
-                </div>
-                <div class="mb-3 row form-group">
-                    <label for="message" class="col-3 col-form-label">Message:</label>
-                    <div class="col-9">
-                        <input type="text" class="form-control" name="message" id="message" />
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Envoyer</button>
-            </form>
-        </div>
-    </div>
+// Récupération des 10 derniers messages
+$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
 
-    <div class="mt-5 container">
-        <?php
-        for ($i = 1; $i <= 10; $i++)
-        {
-        ?>
-            <div class="my-2 row justify-content-center">
-                <div class="col-3"><?php echo $_SESSION['message'][$i]['pseudo']; ?>:</div>
-                <div class="col-9"><?php echo $_SESSION['message'][$i]['message']; ?></div>
-            </div>
-        <?php
-        }
-        ?>
-    </div>
-</body>
+// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+while ($donnees = $reponse->fetch())
+{
+	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+}
 
+$reponse->closeCursor();
+
+?>
+    </body>
 </html>
--- minichat_post.php
+++ minichat_post.php
@@ -1,81 +1,18 @@
 <?php
-// Démarrage de la session
-session_start();
-
+// Connexion à la base de données
 try
 {
-    // On se connecte à la base de donnée MySQL, ici tp_openclassrooms
-    $bdd = new PDO('mysql:host=localhost;dbname=tp_openclassrooms;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 }
-catch (Exception $e)
+catch(Exception $e)
 {
-    // En cas d'erreur, on affiche un message et on arrête tout
-    die('Erreur : ' . $e->getMessage());
+        die('Erreur : '.$e->getMessage());
 }
 
-// On récupère le message insérer par l'utilisateur
+// Insertion du message à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
+$req->execute(array($_POST['pseudo'], $_POST['message']));
 
-if (isset($_POST['pseudo']) AND isset($_POST['message']))
-{
-    $pseudonyme = htmlspecialchars($_POST['pseudo']);
-    $message_laisse = htmlspecialchars($_POST['message']);
-
-    // On insère le message dans la base de donnée
-    $req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message)');
-    $req->execute(array(
-        'pseudo' => $pseudonyme,
-        'message' => $message_laisse,
-    ));
-}
-
-//On récupère tout le contenu de la table minichat dans l'ordre décroissant selon ID
-$reponse = $bdd->query('SELECT * FROM minichat ORDER BY ID DESC');
-
-// On enregistre les 10 derniers messages dans l'array "$_SESSION['message']" qui contient des array "[$nombre_message]"
-
-$nombre_message = 1;
-
-while ($message = $reponse->fetch())
-{    
-    if ($nombre_message <= 10)
-    {
-        $_SESSION['message'][$nombre_message] = $message;
-    }
-    
-    $nombre_message ++;
-}
-
-/*
-
-2e méthode:
-
-$reponse = $bdd->query('SELECT * FROM minichat ORDER BY ID DESC LIMIT 0,10');
-
-$nombre_message = 1;
-
-while ($message = $reponse->fetch())
-{
-    
-    $_SESSION['message'][$nombre_message] = $message;
-    $nombre_message ++;
-}
-*/
-
-// On termine le traitement de la requête
-$reponse->closeCursor();
-
-/*
-while ($message = $reponse->fetch())
-{
-    while ($message['ID'] == $id)
-    {
-        $_SESSION['ID'] = $message['ID'];
-        $_SESSION['pseudo'] = $message['pseudo'];
-        $_SESSION['message'] = $message['message'];
-    }
-}
-*/
-
-// Redirection vers minichat.php
+// Redirection du visiteur vers la page du minichat
 header('Location: minichat.php');
 ?>
